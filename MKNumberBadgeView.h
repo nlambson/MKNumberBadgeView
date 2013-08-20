601634f1a39137ba6c867f0d7fcfa868b8a34320
Nathan Lambson <nlambson@gmail.com>
2013-08-19 19:32:45 -0600

    Fixed warnings. Started working toward arc-compatible code removed synthesize statements removed backwards compatibility; code modified for iOS 7.

--- MKNumberBadgeView.h
+++ MKNumberBadgeView.h
@@ -1,8 +1,7 @@
 //
 //  MKNumberBadgeView.h
-//  MKNumberBadgeView
-//  
-// Copyright 2009 Michael F. Kamprath
+//
+// Copyright 2009-2012 Michael F. Kamprath
 // michael@claireware.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,7 +23,7 @@
 // Use this class to display a badge containing an integer value.Similar to the app icon badges that the iPhone OS permits.
 //
 // Notes:
-//    * When creating the view, the frame used should be larger than the expected visual size of the badge view. Use the alignment 
+//    * When creating the view, the frame used should be larger than the expected visual size of the badge view. Use the alignment
 //      property to control the horizontal placement of the badge within the view's bounds. The badge will always be vertically
 //      centered for the badge itself ignoring the size fo the shadow if it is enabled.
 //    * The view's background color is automatically set to clear. If you change the background color, you may get curious results.
@@ -33,10 +32,7 @@
 #import <UIKit/UIKit.h>
 
 
-@interface MKNumberBadgeView : UIView 
-{
-	NSUInteger _value;
-}
+@interface MKNumberBadgeView : UIView
 
 // The current value displayed in the badge. Updating the value will update the view's display
 @property (assign,nonatomic) NSUInteger value;
@@ -46,6 +42,9 @@
 
 // The offset for the shadow, if there is one.
 @property (assign,nonatomic) CGSize shadowOffset;
+
+// The base color for the shadow, if there is one.
+@property (retain,nonatomic) UIColor * shadowColor;
 
 // Indicates whether the badge view should be drawn with a shine
 @property (assign,nonatomic) BOOL shine;
@@ -60,17 +59,20 @@
 // The color to be used for drawing the stroke around the badge.
 @property (retain,nonatomic) UIColor* strokeColor;
 
+// The width for the stroke around the badge.
+@property (assign,nonatomic) CGFloat strokeWidth;
+
 // The color to be used for drawing the badge's numbers.
 @property (retain,nonatomic) UIColor* textColor;
 
-// How the badge image hould be aligned horizontally in the view. 
-@property (assign,nonatomic) UITextAlignment alignment;
+// How the badge image hould be aligned horizontally in the view.
+@property (assign,nonatomic) CTTextAlignment alignment;
 
 // Returns the visual size of the badge for the current value. Not the same hing as the size of the view's bounds.
 // The badge view bounds should be wider than space needed to draw the badge.
 @property (readonly,nonatomic) CGSize badgeSize;
 
-// The number of pixels between the number inside the badge and the stroke around the badge. This value 
+// The number of pixels between the number inside the badge and the stroke around the badge. This value
 // is approximate, as the font geometry might effectively slightly increase or decrease the apparent pad.
 @property (nonatomic) NSUInteger pad;
 
